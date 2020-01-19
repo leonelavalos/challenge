f9e491f25fed417156c90828f18a352446424f51
leonel <lmavalos@candoit.com.ar>
2020-01-18 21:40:06 -0300

    First commit

---
+++ src/test/java/ar/com/mercadolibre/mutantes/MutantesApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.mercadolibre.mutantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
